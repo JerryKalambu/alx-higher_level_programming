b9b620cdcd6e36f6a8570ea323ceea2694d650f2
JerryKalambu <jeroldkalambu@gmail.com>
2022-11-29 15:42:14 -0800

    tasks

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+#Author - Jerry Kalambu
+
+def pow(a, b):
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Jerry Kalambu
+for letter in range(97, 123);
+    if chr(letter) != 'q' and chr(letter) != 'e';
+        print("{}".format(chr(latter)), end="")
