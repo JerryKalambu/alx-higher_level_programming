45c8f5fff9126e4963cc1976201a19f47b8c3e99
JerryKalambu <jeroldkalambu@gmail.com>
2023-04-10 15:17:42 -0700

    taks

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-lists all states from the database in ascending order
-using MySQLdb
-"""
+lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
